660d618b6c86bd434821a3fbabd5ca9a30700f3e
Radser2001 <supuni.randeniya@gmail.com>
2023-12-02 09:32:29 +0530

    Initial commit

---
+++ HouseRentalApp/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+// import home component
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HomeComponent],
+  // templateUrl: './app.component.html',
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Homes';
+}
